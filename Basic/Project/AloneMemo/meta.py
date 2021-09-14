ea94836608e107c589a07f0da4544befee139434
hellonayeon <hellonayeon@icloud.com>
2021-09-14 16:51:04 +0900

    메타 데이터 스크래핑

---
+++ Basic/Project/AloneMemo/meta.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://movie.naver.com/movie/bi/mi/basic.nhn?code=171539'
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(url,headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 사람이 페이지에 접속해서 보는 메타태그 순서와
+# 파이썬이 페이지 접속해서 보는 메타태그 순서 다르다.
+title = soup.select_one('meta[property="og:title"]')['content']
+image = soup.select_one('meta[property="og:image"]')['content']
+desc = soup.select_one('meta[property="og:description"]')['content']
+print(title, image, desc)
