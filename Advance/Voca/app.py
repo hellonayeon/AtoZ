0943a53ccae579a28e8df8bb95d70d5aba4d0478
hellonayeon <hellonayeon@icloud.com>
2021-09-22 03:27:30 +0900

    사용자 예문 입력 기능 추가

--- Advance/Voca/app.py
+++ Advance/Voca/app.py
@@ -22,7 +22,7 @@ def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token [내토큰]"})
     if r.status_code != 200:
-        return redirect(url_for("main", msg="올바른 단어를 입력해주세요 !"))
+        return redirect(url_for("main", msg="Word not found in dictionary; Try another word"))
     result = r.json()
     print(result)
 
@@ -49,7 +49,37 @@ def delete_word():
     # 단어 삭제하기
     word_receive = request.form["word_give"]
     db.words.delete_one({ "word": word_receive })
+    db.examples.delete_many({"word": word_receive}) # 단어에 대한 예문들도 모두 삭제
     return jsonify({'result': 'success', 'msg': f'단어 {word_receive} 삭제'})
+
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs():
+    word_receive = request.args.get("word_give")
+    result = list(db.examples.find({"word": word_receive}, {"_id": False}))
+    print(result)
+    return jsonify({'result': 'success', 'examples': result})
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    # 예문 저장하기
+    word_receive = request.form["word_give"]
+    ex_receive = request.form["ex_give"]
+    doc = {"word": word_receive, "example": ex_receive}
+    db.examples.insert_one(doc)
+
+    return jsonify({'msg': f'example "{ex_receive}" is saved!'})
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    # 예문 삭제하기
+    word_receive = request.form["word_give"]
+    number_receive = int(request.form["number_give"])
+    example = list(db.examples.find({"word": word_receive}, {"_id": False}))[number_receive]["example"]
+
+    doc = {"word": word_receive, "example": example}
+    db.examples.delete_one(doc)
+
+    return jsonify({'msg': f'example "{example}" is saved!'})
 
 
 if __name__ == '__main__':
