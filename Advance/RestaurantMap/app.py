7936e70aad93adcc668971f6bed6636e409689d6
hellonayeon <hellonayeon@icloud.com>
2021-09-23 00:34:10 +0900

    즐겨찾기 기능 추가

--- Advance/RestaurantMap/app.py
+++ Advance/RestaurantMap/app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('54.180.87.49', 27017, username="test", password="test")
+client = MongoClient('13.209.68.233', 27017, username="test", password="test")
 db = client.dbsparta_advance_week3
 
 
@@ -19,6 +19,20 @@ def get_matjip():
     # 2. 클라이언트 사이드에 돌려주기
     matjip_list = list(db.matjips.find({}, {"_id": False}))
     return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    title_receive = request.form["title_give"]
+    address_receive = request.form["address_give"]
+    action_receive = request.form["action_give"]
+    print(title_receive, address_receive, action_receive)
+
+    if action_receive == "like":
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$set": {"liked": True}})
+    else:
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$unset": {"liked": False}})
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
