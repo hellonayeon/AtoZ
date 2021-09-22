c773834b98885a9d349df90a6e4e6778f73ac8c9
hellonayeon <hellonayeon@icloud.com>
2021-09-22 18:54:50 +0900

    웹사이트 모습 만들기

--- Advance/RestaurantMap/app.py
+++ Advance/RestaurantMap/app.py
@@ -5,18 +5,20 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient('54.180.87.49', 27017, username="test", password="test")
-db = client.dbsparta_plus_week3
+db = client.dbsparta_advance_week3
 
 
 @app.route('/')
 def main():
     return render_template("index.html")
 
-
+# 맛집 목록을 반환하는 API
 @app.route('/matjip', methods=["GET"])
 def get_matjip():
-    # 맛집 목록을 반환하는 API
-    return jsonify({'result': 'success', 'matjip_list': []})
+    # 1. 데이터베이스에서 맛집 목록을 꺼내오기
+    # 2. 클라이언트 사이드에 돌려주기
+    matjip_list = list(db.matjips.find({}, {"_id": False}))
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
